36fbae192ded554dfb7cd8f6503931cb76af207f
sanskar3 <sanskarawasthi9893@gmail.com>
2020-12-23 22:32:09 +0530

    Note App

--- app/src/main/java/com/sanskar/notesapp/MainActivity.kt
+++ app/src/main/java/com/sanskar/notesapp/MainActivity.kt
@@ -2,12 +2,17 @@ package com.sanskar.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NotesRVAdapter.INotesRVAdapter {
 
     lateinit var viewModel: NoteViewModel
 
@@ -15,11 +20,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recycleView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this, this)
+        recycleView.adapter = adapter
 
-     viewModel =ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
-        viewModel.allNotes.observe(this, Observer {  })
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, Observer { list ->
+            list?.let {
+                adapter.updateList(it)
+            }
+        })
 
 
+    }
+
+    override fun onItemClicked(note: Note) {
+
+        viewModel.deleteNote(note)
+        Toast.makeText(this,"${note.text} Deleted",Toast.LENGTH_LONG).show()
+
+
+    }
+
+    fun submitData(view:View){
+        val noteText = input_text.text.toString()
+        if (noteText.isNotEmpty()){
+            viewModel.insertNote(Note(noteText))
+
+            Toast.makeText(this,"${noteText} Deleted",Toast.LENGTH_LONG).show()
+        }
     }
 
 }
